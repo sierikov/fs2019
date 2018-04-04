4ca7a589faf9b5a0773573513f58b474ce6fe345
ArtemSer <artem.sierikov@gmail.com>
2018-04-04 21:42:19 +0300

    Added Titlebar and Menu
    Todo: add bottommenu

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+//import logo from './logo.svg';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {open: false};
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <AppBar
+                    title="Title"
+
+                    iconClassNameRight="muidocs-icon-navigation-expand-more"
+                    onLeftIconButtonClick={() => this.setState({open: !this.state.open})}
+                />
+                <Drawer open={this.state.open}
+                        docked={false}
+                        onRequestChange={(open) => this.setState({open})}
+                >
+                    <MenuItem> Menu Item </MenuItem>
+                    <MenuItem> Menu Item </MenuItem>
+                </Drawer>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { MuiThemeProvider } from 'material-ui/styles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MuiThemeProvider><App /></MuiThemeProvider>, document.getElementById('root'));
 registerServiceWorker();
