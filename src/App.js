f3ca90b7d1bc0f3019cb2b755f2c6422dc229767
ArtemSer <artem.sierikov@gmail.com>
2018-04-09 23:58:38 +0300

    Added menu tools & react DOM

--- src/App.js
+++ src/App.js
@@ -1,39 +1,108 @@
-import React, {Component} from 'react';
-import './App.css';
-//import logo from './logo.svg';
-import AppBar from 'material-ui/AppBar';
-import Drawer from 'material-ui/Drawer';
-import MenuItem from 'material-ui/MenuItem';
-import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
-import injectTapEventPlugin from 'react-tap-event-plugin';
+import React, { Component } from 'react'
+import './App.css'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer'
+import MenuItem from 'material-ui/MenuItem'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import fusTheme from './fusTheme'
 
 injectTapEventPlugin();
 
 class App extends Component {
-    constructor(props) {
+    constructor (props) {
         super(props);
-        this.state = {open: false};
+        this.state = { open: false }
     }
-
-    render() {
+    render () {
         return (
-            <div className="App">
-                <AppBar
-                    title="THE BASE"
+            <BrowserRouter>
+                <MuiThemeProvider muiTheme={getMuiTheme(fusTheme)}>
+                    <div>
+                        <AppBar
+                            title='Title'
+                            iconClassNameRight='muidocs-icon-navigation-expand-more'
+                            onLeftIconButtonTouchTap={() =>
+                                this.setState({ open: !this.state.open })}
+                        />
+                        <Drawer
+                            open={this.state.open}
+                            docked={false}
+                            onRequestChange={open => this.setState({ open })}
+                        >
+                            <MenuItem>
+                                <Link to='/'>Home</Link>
+                            </MenuItem>
+                            <MenuItem>
+                                <Link to='/logos'>Logos</Link>
+                            </MenuItem>
+                            <MenuItem>
+                                <Link to='/posters'>Posters</Link>
+                            </MenuItem>
+                            <MenuItem>
+                                <Link to='/letterhead'>Letterhead</Link>
+                            </MenuItem>
+                            <MenuItem>
+                                <Link to='/share-a-story'>Share a Story</Link>
+                            </MenuItem>
+                            <MenuItem>
+                                <Link to='/service-request-form'>Service Request Form</Link>
+                            </MenuItem>
+                            <MenuItem>
+                                <Link to='/tutorial'>Tutorial</Link>
+                            </MenuItem>
+                        </Drawer>
 
-                    iconClassNameRight="muidocs-icon-navigation-expand-more"
-                    onLeftIconButtonClick={() => this.setState({open: !this.state.open})}
-                />
-                <Drawer open={this.state.open}
-                        docked={false}
-                        onRequestChange={(open) => this.setState({open})}
-                >
-                    <MenuItem> Menu Item </MenuItem>
-                    <MenuItem> Menu Item </MenuItem>
-                </Drawer>
-            </div>
-        );
+                        <Route exact path='/' render={() => <h1>Home View</h1>} />
+                        <Route
+                            exact
+                            path='/logos-posters'
+                            render={() => <h1>Logos and Posters View</h1>}
+                        />
+                        <Route exact path='/logos' render={() => <h1>Logos View</h1>} />
+                        <Route exact path='/posters' render={() => <h1>Posters View</h1>} />
+                        <Route
+                            exact
+                            path='/letterhead'
+                            render={() => <h1>Letterhead View</h1>}
+                        />
+                        <Route
+                            exact
+                            path='/share-a-story'
+                            render={() => <h1>Share a Story View</h1>}
+                        />
+                        <Route
+                            exact
+                            path='/planning-guide'
+                            render={() => <h1>Planning Guide View</h1>}
+                        />
+                        <Route
+                            exact
+                            path='/services'
+                            render={() => <h1>Services View</h1>}
+                        />
+                        <Route
+                            exact
+                            path='/glossary'
+                            render={() => <h1>Glossary View</h1>}
+                        />
+                        <Route
+                            exact
+                            path='/service-request-form'
+                            render={() => <h1>Service Request Form View</h1>}
+                        />
+                        <Route
+                            exact
+                            path='/tutorial'
+                            render={() => <h1>Tutorial View</h1>}
+                        />
+                    </div>
+                </MuiThemeProvider>
+            </BrowserRouter>
+        )
     }
 }
 
-export default App;
+export default App
--- src/fusTheme.js
+++ src/fusTheme.js
@@ -1,19 +1,39 @@
-import  ColorManipulation from 'material-ui/lib/utils/color-manipulator';
-import * as Colors from "material-ui/styles/colors";
+/**
+ * More info here: http://www.material-ui.com/#/customization/themes
+ */
+
+import {
+    cyan500,
+    cyan700,
+    pinkA200,
+    grey100,
+    grey300,
+    grey400,
+    grey500,
+    white,
+    darkBlack,
+    fullBlack
+} from 'material-ui/styles/colors'
+import { fade } from 'material-ui/utils/colorManipulator'
+import spacing from 'material-ui/styles/spacing'
 
 export default {
+    spacing: spacing,
+    fontFamily: 'Roboto, sans-serif',
     palette: {
-        primary1Color: Colors.cyan500,
-        primary2Color: Colors.cyan700,
-        primary3Color: Colors.lightBlack,
-        accent1Color: Colors.pinkA200,
-        accent2Color: Colors.grey100,
-        accent3Color: Colors.grey500,
-        textColor: Colors.deepPurpleA700,
-        alternateTextColor: Colors.white,
-        canvasColor: Colors.white,
-        borderColor: Colors.grey300,
-        disabledColor: ColorManipulation.fade(Colors.darkBlack, 0.3),
-        pickerHeaderColor: Colors.cyan500,
+        primary1Color: '#21412a',
+        primary2Color: cyan700,
+        primary3Color: grey400,
+        accent1Color: pinkA200,
+        accent2Color: grey100,
+        accent3Color: grey500,
+        textColor: darkBlack,
+        alternateTextColor: white,
+        canvasColor: white,
+        borderColor: grey300,
+        disabledColor: fade(darkBlack, 0.3),
+        pickerHeaderColor: cyan500,
+        clockCircleColor: fade(darkBlack, 0.07),
+        shadowColor: fullBlack
     }
-};
+}
